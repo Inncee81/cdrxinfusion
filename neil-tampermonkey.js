b7b7c38fc7f6ea405b1fb6f32952f6ad6db6be03
ddgiants <ddgiants@gmail.com>
2021-02-05 08:39:56 -0500

    Add files via upload

---
+++ neil-tampermonkey.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         cdrxinfusion carecentrix quick grab
+// @namespace    https://www.carecentrixportal.com/ProviderPortal/referral/myReferral.do
+// @version      1.0
+// @description  Grab that patient!
+// @author       Darren DeLitizia ddgiants@gmail.com
+// @include      https://www.carecentrixportal.com/ProviderPortal/referral/myReferral.do
+// @include      https://www.carecentrixportal.com/ProviderPortal/referral/myReferral.do?method1=selectTab&hideAdvanceSearchFlag=true&activeTab=Active*
+// @include      https://www.carecentrixportal.com/ProviderPortal/referral/notificationReferral.do*
+// @grant        none
+
+// ==/UserScript==
+
+$(document).ready(function() {
+  var loc = window.top.location.toString();
+  if(loc.indexOf('btnValue=acceptSubmit')!==-1){
+    window.location.href = "https://www.carecentrixportal.com/ProviderPortal/referral/myReferral.do";
+    //window.top.location.replace(loc.replace(/\?btnValue=acceptSubmit/g,''));
+  }
+
+  refreshClick();
+  function refreshClick() {
+    var x = 1;
+
+    // if a new patient drops click the view link
+    var DDmatchingLinks = $( "a[href*='/ProviderPortal/referral/notificationReferral.do']" );
+
+    // once the view link is clicked click the accept button
+    if($("#btnAccept").length != 0) {
+      $("#btnAccept").click();
+      // once the accept button is clicked click the checkbox in the popup
+      $("#OK").click();
+      $("#SOC").click();// this is the checkbox
+      $("#OK").click();
+      $("#submitButton").click();
+    }
+
+    else if(DDmatchingLinks.length > 0) {
+      $(DDmatchingLinks).each(function(index) {
+        window.location.replace($(this).attr("href"));
+      });
+    }
+
+    // if no new patients continue checking screen every second
+    else {
+      setTimeout(function(){ location.reload(); }, x*1000);
+    }
+  }
+});
