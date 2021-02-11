d5d2edf520bbd881bf923930b3dd1265f45b7438
ddgiants <ddgiants@gmail.com>
2021-02-11 08:57:16 -0500

    Update neil-tampermonkey.js

--- neil-tampermonkey.js
+++ neil-tampermonkey.js
@@ -10,32 +10,62 @@
 // @grant        none
 
 // ==/UserScript==
-
 $(document).ready(function() {
+  // get current location. If it includes submit - redirect to main page
   var loc = window.top.location.toString();
 
-  refreshClick();
-  function refreshClick() {
+  checkPage();
+  function checkPage() {
     var x = 1.5;
 
-    // if a new patient drops click the view link
-    var DDmatchingLinks = $( "a[href*='/ProviderPortal/referral/notificationReferral.do']" );
+    // if a new patient drops set the link variable
+    var matchingLinks = $( "a[href*='/ProviderPortal/referral/notificationReferral.do']" );
 
     // once the view link is clicked click the accept button
-    if($("#btnAccept").length != 0) {
-      $("#btnAccept").click();
-      // once the accept button is clicked click the checkbox in the popup
-      $("#OK").click();
-      $("#SOC").click();// this is the checkbox
-      $("#OK").click();
-      $("#submitButton").click();
+    // HCPC would be eq(2)
+    // Diagnosis would be eq(6)
+    if($("#htdBtnValue").length != 0) {
+      var rejectBit = 0;
+      var badHCPC = ["J3411","J2405","S9366","S9365","S9367"];
+      var badDiagnosis = ["D509"];
+      $(".myreferralviewassigndetails > table > tbody > tr").each(function() {
+        var hcpc = $.trim($(this).find("td:eq(2)").html());
+        if(hcpc != undefined) {
+          if(badHCPC.includes(hcpc) == false) {
+            var diagnosisTd = $(this).find("td:eq(6)");
+            var diagnosis = $.trim($(diagnosisTd).find("div").html());
+            if(diagnosis != undefined) {
+              if(badDiagnosis.includes(diagnosis) == true) {
+                rejectBit = 1;
+              }
+            }// if diagnosis is undefined
+          }
+          else {
+            rejectBit = 1;
+          }
+        }// if hcpc is undefined
+      });// end each tr
+      if(rejectBit == 1) {
+        refreshPage(x);
+      }
+      else {
+        $("#btnAccept").click();
+        // once the accept button is clicked click the checkbox in the popup
+        $("#OK").click();
+        $("#SOC").click();// this is the checkbox
+        $("#OK").click();
+        $("#submitButton").click();
+      }
     }
 
-    else if(DDmatchingLinks.length > 0) {
-      $(DDmatchingLinks).each(function(index) {
+    // if the view link exsists, click it
+    else if(matchingLinks.length > 0) {
+      $(matchingLinks).each(function(index) {
         window.location.replace($(this).attr("href"));
       });
     }
+
+    // if the accept but was clicked and we accepted the patient, then redirect to the main page
     else if(loc.indexOf('btnValue=acceptSubmit')!==-1){
       setTimeout(function(){ window.location.href = "https://www.carecentrixportal.com/ProviderPortal/referral/myReferral.do"; }, 3000);
       //window.top.location.replace(loc.replace(/\?btnValue=acceptSubmit/g,''));
@@ -43,7 +73,13 @@ $(document).ready(function() {
 
     // if no new patients continue checking screen every second
     else {
-      setTimeout(function(){ location.reload(); }, x*1000);
+      refreshPage(x);
     }
+  }// end function
+
+  function refreshPage(x) {
+    setTimeout(function(){ location.reload(); }, x*1000);
   }
+
+
 });
